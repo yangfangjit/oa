df9d0fcebe31a2e6e515d1653e359d44a85e330b
francisyang <francisyang@192.168.1.108>
2017-08-01 14:00:11 +0800

    modified dao configuration

--- src/main/java/com/banana/oa/spring/DaoConfig.java
+++ src/main/java/com/banana/oa/spring/DaoConfig.java
@@ -19,7 +19,7 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 @PropertySource("classpath:jdbc.properties")
-public class BeanConfig implements EnvironmentAware {
+public class DaoConfig implements EnvironmentAware {
 
 	private Environment env;
 	
